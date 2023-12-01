38af71292cfa16a81f44a995b314c261659f33ea
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-30 19:38:42 -0500

    Update layout.rs

--- src/core/layout.rs
+++ src/core/layout.rs
@@ -14,6 +14,39 @@ impl LayoutManager for VerticalLayout {
     }
 }
 
+// Define a basic horizontal layout manager
+struct HorizontalLayout;
+
+impl LayoutManager for HorizontalLayout {
+    fn arrange_widgets(&self, widgets: &[&dyn Widget]) {
+        for widget in widgets {
+            widget.render();
+        }
+    }
+}
+
+// Define a basic grid layout manager
+struct GridLayout {
+    columns: usize,
+}
+
+impl GridLayout {
+    fn new(columns: usize) -> Self {
+        Self { columns }
+    }
+}
+
+impl LayoutManager for GridLayout {
+    fn arrange_widgets(&self, widgets: &[&dyn Widget]) {
+        for (index, widget) in widgets.iter().enumerate() {
+            widget.render();
+            if (index + 1) % self.columns == 0 {
+                println!(); // Start a new row
+            }
+        }
+    }
+}
+
 /*
 fn main() {
     // Create instances of widgets
@@ -24,5 +57,16 @@ fn main() {
     // Arrange widgets using the vertical layout manager
     let vertical_layout = VerticalLayout;
     let widgets: Vec<&dyn Widget> = vec![&label, &button, &text_box];
+    println!("Vertical Layout:");
     vertical_layout.arrange_widgets(&widgets);
+
+    // Arrange widgets using the horizontal layout manager
+    let horizontal_layout = HorizontalLayout;
+    println!("Horizontal Layout:");
+    horizontal_layout.arrange_widgets(&widgets);
+
+    // Arrange widgets using the grid layout manager
+    let grid_layout = GridLayout::new(2);
+    println!("Grid Layout (2 columns):");
+    grid_layout.arrange_widgets(&widgets);
 }*/
