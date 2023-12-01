9a6e718b5bf22fb0d640f9cd558d069c377c3bf0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-30 21:11:25 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 mod core;
 use core::gust::Gust;
-use winit::{
-    event_loop::{EventLoop}
-};
 
 fn main() {
-    let event_loop = EventLoop::new();
     let mut gust = Gust::new();
-    gust.run(event_loop);
+    gust.run();
 }
