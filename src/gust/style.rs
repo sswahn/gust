d8a9bae043e85ee7f0a868b22bf5017946062f97
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-01 15:23:44 -0500

    Update style.rs

--- src/gust/style.rs
+++ src/gust/style.rs
@@ -1,16 +1,16 @@
 // Define a struct for widget styling
 struct Styles {
-    text_color: String,
-    background_color: String,
-    font_size: f32,
+    text_color: Option<String>,
+    background_color: Option<String>,
+    font_size: Option<f32>,
 }
 
 impl Styles {
     fn new(styles: HashMap<String, String>) -> Self {
         Self {
-            text_color: styles.text_color,
-            background_color: styles.background_color,
-            font_size: styles.font_size,
+            text_color: styles.get("text_color").cloned(),
+            background_color: styles.get("background_color").cloned(),
+            font_size: styles.get("font_size").cloned(),
         }
     }
 }
