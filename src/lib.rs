4cbadd0d024b42533947cb94e004c17f1cb01ae6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-01 17:57:57 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
-mod gust;
-pub use gust::{Gust, Layout, Widgets};
+pub mod gust;
+
+// Re-export layout-related items
+pub mod layout {
+    pub use crate::layout::vertical_layout::VerticalLayout;
+    pub use crate::layout::horizontal_layout::HorizontalLayout;
+    pub use crate::layout::grid_layout::GridLayout;
+}
+
+// Re-export widget-related items
+pub mod widgets {
+    pub use crate::widgets::button::Button;
+    pub use crate::widgets::label::Label;
+    pub use crate::widgets::text_box::TextBox;
+    pub use crate::widgets::menu::Menu;
+    pub use crate::widgets::menu_item::MenuItem;
+}
