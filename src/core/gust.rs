ce49c8c397a3155b359ac6d63471defeca81bb8f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-30 23:27:34 -0500

    Update gust.rs

--- src/core/gust.rs
+++ src/core/gust.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::error::Error;
 use winit::{
-    event::{Event, WindowEvent},
+    event::{Event, WindowEvent, VirtualKeyCode, ElementState, MouseButton},
     event_loop::{ControlFlow, EventLoop},
     platform::windows::WindowBuilderExtWindows,
     window::WindowBuilder,
@@ -17,12 +17,6 @@ struct Window {
     id: winit::window::WindowId,
     title: String,
     is_minimized: bool,
-}
-
-enum CustomEvent {
-    ButtonClick,
-    TextChange(String),
-    // Add more custom events as needed
 }
 
 impl Gust {
@@ -52,31 +46,34 @@ impl Gust {
     }
 
 
-    fn handle_mouse_click(&mut self) {
-        self.count += 1;
-        println!("Button clicked: {}", self.count);
-    
-        // Emit a custom button click event
-        self.handle_event(CustomEvent::ButtonClick);
-    }
-
-
-    fn handle_event(&self, event: CustomEvent) {
-        // Handle user-defined custom events
-        if let Some(handler) = self.custom_event_handlers.get(&event) {
-            handler();
+    fn handle_event(&mut self, event: &Event<()>, control_flow: &mut ControlFlow) {
+        match event {
+            Event::WindowEvent { event, window_id, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    self.windows.retain(|window| window.id != *window_id);
+                    if self.windows.is_empty() {
+                        *control_flow = ControlFlow::Exit;
+                    }
+                }
+                WindowEvent::Resized(_) => {
+                    // Handle resize event if needed
+                }
+                WindowEvent::MouseInput { state, button, .. } if *state == ElementState::Pressed => {
+                    self.handle_mouse_click();
+                }
+                WindowEvent::KeyboardInput { input, .. } => {
+                    if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
+                        *control_flow = ControlFlow::Exit;
+                    }
+                }
+                _ => (),
+            },
+            Event::UserEvent(user_event) => {
+                // Handle other predefined user events if needed
+            }
+            _ => (),
         }
     }
-
-    fn add_event_handler(&mut self, event: CustomEvent, handler: Box<dyn Fn()>) {
-        self.custom_event_handlers.insert(event, handler);
-    }
-
-    /* Example of adding a custom event handler
-    gust.add_event_handler(CustomEvent::ButtonClick, Box::new(|| {
-        println!("Handling custom button click event!");
-    }));
-    */
 
 
     fn run(&mut self) {
@@ -84,33 +81,9 @@ impl Gust {
         self.create_window(&event_loop, "Main Window").unwrap_or_else(|err| {
             eprintln!("Error creating window: {}", err);
         });
-    
-        self.run_event_loop(event_loop);
-    }
-
-    fn run_event_loop(&mut self, mut event_loop: EventLoop<()>) {
         event_loop.run(move |event, _, control_flow| {
             *control_flow = ControlFlow::Wait;
-
-            match &event {
-                Event::WindowEvent { event, window_id } => {
-                    self.windows.retain(|window| window.id != *window_id);
-                    if self.windows.is_empty() {
-                        *control_flow = ControlFlow::Exit;
-                    }
-                }
-                Event::UserEvent(user_event) => {
-                    self.handle_custom_event(*user_event); // Handle user-defined custom events
-                }
-                Event::WindowEvent::MouseInput => {
-                    self.handle_mouse_click();
-                }
-                _ => (),
-            }
+            self.handle_event(&event, control_flow);
         });
-    }
-
-    fn is_main_window(&self, window_id: winit::window::WindowId) -> bool {
-        self.windows.iter().any(|window| window.id == window_id)
     }
 }
