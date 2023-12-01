4972b7a5f31eca5db0572fff0e62e374e7ce9d88
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-30 20:16:33 -0500

    Update widgets.rs

--- src/core/widgets.rs
+++ src/core/widgets.rs
@@ -1,5 +1,22 @@
 use std::rc::Rc;
 
+// Define a struct for widget styling
+struct Style {
+    text_color: String,
+    background_color: String,
+    font_size: u16,
+}
+
+impl Style {
+    fn new(text_color: &str, background_color: &str, font_size: u16) -> Self {
+        Self {
+            text_color: text_color.to_string(),
+            background_color: background_color.to_string(),
+            font_size,
+        }
+    }
+}
+
 // Define a trait for common properties and behavior of widgets
 trait Widget {
     fn render(&self);
@@ -129,6 +146,7 @@ impl Widget for Menu {
         }
     }
 }
+
 
 /*
 fn main() {
