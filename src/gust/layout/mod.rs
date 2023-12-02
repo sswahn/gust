9dbcc8d34b205d88bc917cca928fdc980477e23a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-02 14:37:12 -0500

    Update mod.rs

--- src/gust/layout/mod.rs
+++ src/gust/layout/mod.rs
@@ -1,3 +1,7 @@
-pub mod vertical_layout;
-pub mod horizontal_layout;
-pub mod grid_layout;
+mod horizontal_layout;
+mod vertical_layout;
+mod grid_layout;
+
+pub use horizontal_layout::HorizontalLayout;
+pub use vertical_layout::VerticalLayout;
+pub use grid_layout::GridLayout;
