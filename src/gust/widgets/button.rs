41e20e627d282c38aa3f89442e233538010c733e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-01 20:53:14 -0500

    Create button.rs

---
+++ src/gust/widgets/button.rs
@@ -0,0 +1,32 @@
+use std::rc::Rc;
+mod widget;
+use widget::Widget;
+
+// Define a basic button widget
+struct Button {
+    label: String,
+    styles: Option<Styles>,
+    on_click: Rc<dyn Fn()>,
+}
+
+impl Button {
+    fn new(label: &str, on_click: Rc<dyn Fn()>) -> Self {
+        Self {
+            label: label.to_string(),
+            styles: None,
+            on_click,
+        }
+    }
+    fn set_styles(&mut self, styles: HashMap<String, String>) {
+        self.style = Some(styles);
+    }
+    fn click(&self) {
+        (self.on_click)();
+    }
+}
+
+impl Widget for Button {
+    fn render(&self) {
+        println!("Button: {}", self.label);
+    }
+}
