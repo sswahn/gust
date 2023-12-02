b1962b555e8a9981e6c34d44ccaf8286b517007a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-01 20:07:58 -0500

    Create grid.rs

---
+++ src/gust/layout/grid.rs
@@ -0,0 +1,24 @@
+mod layout_manager;
+use layout_manager::LayoutManager;
+
+// Define a basic grid layout manager
+struct GridLayout {
+    columns: usize,
+}
+
+impl GridLayout {
+    fn new(columns: usize) -> Self {
+        Self { columns }
+    }
+}
+
+impl LayoutManager for GridLayout {
+    fn arrange_widgets(&self, widgets: &[&dyn Widget]) {
+        for (index, widget) in widgets.iter().enumerate() {
+            widget.render();
+            if (index + 1) % self.columns == 0 {
+                println!(); // Start a new row
+            }
+        }
+    }
+}
