f2e0aac632efc24fa92e70a9109ca759a4f4fb84
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-01 20:00:58 -0500

    Update vertical_layout.rs

--- src/gust/layout/vertical_layout.rs
+++ src/gust/layout/vertical_layout.rs
@@ -1,7 +1,5 @@
-// Define a trait for layout managers
-trait LayoutManager {
-    fn arrange_widgets(&self, widgets: &[&dyn Widget]);
-}
+mod layout_manager;
+use layout_manager::LayoutManager;
 
 // Define a basic vertical layout manager
 struct VerticalLayout;
@@ -13,60 +11,3 @@ impl LayoutManager for VerticalLayout {
         }
     }
 }
-
-// Define a basic horizontal layout manager
-struct HorizontalLayout;
-
-impl LayoutManager for HorizontalLayout {
-    fn arrange_widgets(&self, widgets: &[&dyn Widget]) {
-        for widget in widgets {
-            widget.render();
-        }
-    }
-}
-
-// Define a basic grid layout manager
-struct GridLayout {
-    columns: usize,
-}
-
-impl GridLayout {
-    fn new(columns: usize) -> Self {
-        Self { columns }
-    }
-}
-
-impl LayoutManager for GridLayout {
-    fn arrange_widgets(&self, widgets: &[&dyn Widget]) {
-        for (index, widget) in widgets.iter().enumerate() {
-            widget.render();
-            if (index + 1) % self.columns == 0 {
-                println!(); // Start a new row
-            }
-        }
-    }
-}
-
-/*
-fn main() {
-    // Create instances of widgets
-    let label = Label::new("Hello, Rust!");
-    let button = Button::new("Click me", Rc::new(|| println!("Button clicked!")));
-    let text_box = TextBox::new(Rc::new(|text| println!("Text changed: {}", text)));
-
-    // Arrange widgets using the vertical layout manager
-    let vertical_layout = VerticalLayout;
-    let widgets: Vec<&dyn Widget> = vec![&label, &button, &text_box];
-    println!("Vertical Layout:");
-    vertical_layout.arrange_widgets(&widgets);
-
-    // Arrange widgets using the horizontal layout manager
-    let horizontal_layout = HorizontalLayout;
-    println!("Horizontal Layout:");
-    horizontal_layout.arrange_widgets(&widgets);
-
-    // Arrange widgets using the grid layout manager
-    let grid_layout = GridLayout::new(2);
-    println!("Grid Layout (2 columns):");
-    grid_layout.arrange_widgets(&widgets);
-}*/
