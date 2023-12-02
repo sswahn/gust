4607a877cb823a21419f9f487a39b0312f5552df
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-01 21:20:24 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,11 @@
 // Import the internal modules
 mod gust;
 
-// Re-export the main types or modules
+// Re-export the main types
 pub use gust::Gust;
 
 // Re-export layout modules
-pub mod layout {
-    pub use gust::layout::{VerticalLayout, HorizontalLayout, GridLayout};
-}
+pub use gust::layout::{VerticalLayout, HorizontalLayout, GridLayout};
 
 // Re-export widgets modules
-pub mod widgets {
-    pub use gust::widgets::{Button, Label, TextBox, Menu, MenuItem};
-}
-
+pub use gust::widgets::{Button, Label, TextBox, Menu, MenuItem};
