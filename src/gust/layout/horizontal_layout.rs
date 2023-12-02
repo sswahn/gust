0d3d2bc39d55eea39a8fe5fe44bc2edc68e3f9fb
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-02 12:51:32 -0500

    Update horizontal_layout.rs

--- src/gust/layout/horizontal_layout.rs
+++ src/gust/layout/horizontal_layout.rs
@@ -2,7 +2,7 @@ mod layout_manager;
 use layout_manager::LayoutManager;
 
 // Define a basic horizontal layout manager
-struct HorizontalLayout;
+pub struct HorizontalLayout;
 
 impl LayoutManager for HorizontalLayout {
     fn arrange_widgets(&self, widgets: &[&dyn Widget]) {
