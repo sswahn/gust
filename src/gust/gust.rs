24c33515245500cc3ad278a75a845049ce030a9e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-12-03 19:26:09 -0500

    Update gust.rs

--- src/gust/gust.rs
+++ src/gust/gust.rs
@@ -64,7 +64,7 @@ impl Gust {
                         }
                     }
                     WindowEvent::KeyboardInput { event, .. } => {
-                        println!("event.physical_key: {}", event.physical_key);
+                        println!("event.physical_key: {}", event.physical_key.to_string());
 
                         if let Some(keycode) = event.physical_key {
                             match keycode {
